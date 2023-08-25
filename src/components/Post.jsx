f1e8fd4a4ced50d7be29a38ad8e7b5d611abe1b0
Razvan Bulgaru <razvan.bulgaru@gmail.com>
2023-08-25 15:57:30 +0300

    Create header and image sections of the post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,23 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react';
+import { BiDotsHorizontalRounded } from 'react-icons/bi';
 
 const Post = ({ post }) => {
 	const { username, id, userImg, img, caption } = post;
 	return (
-		<div>
-			<h1></h1>
+		<div className="bg-white my-7 border rounded-md">
+			{/* Post header */}
+			<div className="flex items-center p-5">
+				<img
+					src={userImg}
+					alt={username}
+					className="h-12 rounded-full object-cover border p-1 mr-3"
+				/>
+				<p className="font-bold flex-1">{username}</p>
+				<BiDotsHorizontalRounded className="text-2xl" />
+			</div>
+			{/* Post Image */}
+			<img src={img} alt={caption} className="object-cover w-full" />
 		</div>
 	);
 };
