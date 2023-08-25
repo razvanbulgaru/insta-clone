c761c289119ab1891881f63fedb6247e8a34fe3d
Razvan Bulgaru <razvan.bulgaru@gmail.com>
2023-08-25 16:28:23 +0300

    Make feed component responsive

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -4,14 +4,14 @@ import Posts from './Posts';
 
 const Feed = () => {
 	return (
-		<main>
-			<section>
+		<main className="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto">
+			<section className="md:col-span-2">
 				{/* Stories */}
 				<Stories />
 				{/* Posts */}
 				<Posts />
 			</section>
-			<section>
+			<section className="hidden md:inline-grid md:col-span-1">
 				{/* Mini profile */}
 				{/* Suggestions */}
 			</section>
