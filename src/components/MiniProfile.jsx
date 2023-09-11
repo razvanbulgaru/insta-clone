a46035ea5abd2d1ef7770d3d715a5061f5318ae4
Razvan Bulgaru <razvan.bulgaru@gmail.com>
2023-09-11 09:19:13 +0300

    Complete user log in

--- src/components/MiniProfile.jsx
+++ src/components/MiniProfile.jsx
@@ -1,19 +1,25 @@
 /* eslint-disable @next/next/no-img-element */
+'use client';
 import React from 'react';
+import { useSession, signOut } from 'next-auth/react';
 
 const MiniProfile = () => {
+	const { data: session } = useSession();
 	return (
 		<div className="flex items-center justify-between mt-14 ml-10">
 			<img
-				src="https://media.licdn.com/dms/image/C4E03AQHkdBvprGDBBw/profile-displayphoto-shrink_800_800/0/1622011196095?e=2147483647&v=beta&t=Ey11EN9bSLaWodByp9757mbYfvpoSX-Pc5MQpi998yo"
+				src={session?.user.image}
 				alt="User image"
 				className="h-16 rounded-full border p-[2px]"
 			/>
 			<div className="flex-1 ml-4">
-				<h2 className="font-bold">razvan901206</h2>
+				<h2 className="font-bold">{session?.user.username}</h2>
 				<h3 className="text-sm text-gray-400">Welcome to instagram</h3>
 			</div>
-			<button className="font-semibold text-blue-400 text-sm">
+			<button
+				onClick={signOut}
+				className="font-semibold text-blue-400 text-sm"
+			>
 				Sign out
 			</button>
 		</div>
--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,14 +1,18 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react';
+import { HiPlus } from 'react-icons/hi';
 
-const Story = ({ username, img }) => {
+const Story = ({ username, img, isUser }) => {
 	return (
-		<div className="group cursor-pointer">
+		<div className="group cursor-pointer relative">
 			<img
 				src={img}
 				alt={username}
 				className="h-14 rounded-full p-[1.5px] border-2 border-red-500 group-hover:scale-110 transition-transform duration-200 ease-out"
 			/>
+			{isUser && (
+				<HiPlus className="text-white absolute top-4 left-4 text-2xl group-hover:scale-110 transition-transform duration-200 ease-out" />
+			)}
 			<p className="text-xs w-14 truncate">{username}</p>
 		</div>
 	);
